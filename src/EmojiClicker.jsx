26a030cb3659b7d59e1ddb9e47c943c1a91916eb
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-09-17 15:18:41 -0500

    emojis deleted

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState(['😭']);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: '😭' }]);
   function addEmoji() {
-    setEmojis((prev) => [...prev, '🍻']);
+    setEmojis((prev) => [...prev, { id: uuid(), emoji: '🍻' }]);
+  }
+  function deleteEmoji(id) {
+    setEmojis((prev) => {
+      return prev.filter((e) => e.id !== id);
+    });
   }
   return (
     <div>
       {emojis.map((e) => (
-        <h2>{e}</h2>
+        <h2 onClick={() => deleteEmoji(e.id)} key={e.id}>
+          {e.emoji}
+        </h2>
       ))}
       <button onClick={addEmoji}>Add Emoji</button>
     </div>
