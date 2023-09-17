f11c0cbe287c6d294bbccc568c4688867eeda3bb
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-09-17 12:49:25 -0500

    made real progress

--- src/Dumbo.jsx
+++ src/Dumbo.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
 
+function generateGameBoard() {
+  console.log('making game right now');
+  return Array(5000);
+}
+
 export default function Dumbo() {
-  return <div>Dumbo</div>;
+  const [board, setBoard] = useState(generateGameBoard());
+  return (
+    <div>
+      <button onClick={() => setBoard('anything')}>CHANGE STATE</button>
+    </div>
+  );
 }
